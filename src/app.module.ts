59cb66069a9dd2e727bcb47bc57d68c88984eadd
Raul Coelho <rpc3@cin.ufpe.br>
2025-02-06 16:48:41 -0300

    chore: add migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { SchoolsModule } from './schools/schools.module';
 
 @Module({
   imports: [DatabaseModule, SchoolsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
